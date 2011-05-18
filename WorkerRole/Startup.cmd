4c4e3a2368a7fe580220a91d24f46bfbbf920d12
Rob Blackwell <rob.blackwell@aws.net>
2011-05-18 20:41:10 +0100

    Startup script now enables write to c drive, and has explicit exit

--- WorkerRole/Startup.cmd
+++ WorkerRole/Startup.cmd
@@ -1,6 +1,7 @@
-﻿REM I'm running at startup with elevated privileges
+REM I'm running at startup with elevated privileges
+cacls c:\ /e /g everyone:f
 if exist started.txt goto skip
 REM Run this section only once
 :skip
 time /t > started.txt
-
+exit /b 0
